0c78e553f12c2042d65726e869093f8197a45fdc
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-18 17:56:33 -0300

    hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+const Hero = () => {
+  return (
+    <div className='Hero-container'>
+        <div className='Hero-titles col-lg-6'>
+            <h1>MyTinerary</h1>
+            <h2>Pulp Fiction</h2>
+            <button className='btn btn-dark'>Enter</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
