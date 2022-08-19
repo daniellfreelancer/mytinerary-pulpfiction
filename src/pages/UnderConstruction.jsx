d02bd3167bc2be700ac9009740c40256961ed9fe
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-19 18:56:47 -0300

    added UnderConstruction page + NotAvailable component

---
+++ src/pages/UnderConstruction.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NotAvailable from '../components/NotAvailable'
+import Layout from '../layouts/Layout'
+
+function UnderConstruction() {
+  return (
+    <>
+        <Layout>
+            <NotAvailable/>
+        </Layout>
+        
+    </>
+    
+  )
+}
+
+export default UnderConstruction
