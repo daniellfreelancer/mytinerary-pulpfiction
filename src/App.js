372a122c7d3512ad49ff6e090a174be3d03c24ab
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-22 17:11:36 -0300

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Cities from './pages/Cities';
+// import Carousel from './components/Carousel';
+// import Footer from './components/Footer';
+// import Hero from './components/Hero';
+// import Cities from './pages/Cities';
 import NewCities from './pages/NewCities';
-import UnderConstruction from './pages/UnderConstruction';
-import Welcome from './pages/Welcome';
+// import UnderConstruction from './pages/UnderConstruction';
+// import Welcome from './pages/Welcome';
 
 function App() {
 
