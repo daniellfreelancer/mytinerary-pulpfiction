b41af9903d96d0388808168fa9617defb2e85b00
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-22 16:16:40 -0300

    update components/styles for carousel rendering

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,24 +1,83 @@
-import React from 'react'
-import '../App.css'
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import ButtonCarousel from "./ButtonCarousel";
+
 
 function Carousel(props) {
+  const arrayCity = props.cities;
+  const range = props.range
+  const [start, setStart] = useState(0)
+  const [end, setEnd] = useState(start + range);
+  const maxSlider = (props.slides * range)
+  const timeSlider = 5000
 
-    const arrayCity = props.cities
+  // Slider auto
+
+  const [intervalID, setIntervalID] = useState();
+
+
+
+
+  useEffect(() => {
+
+    let id = setInterval(()=>{
+
+      previous()
+
+    },timeSlider)
+
+    setIntervalID(id)
+
+    return () =>{
+      clearInterval(intervalID)
+    }
+
+  }, [start]);
+
+
+
+  //variables end y start
+
+  function next(){
+     if(start < maxSlider - range){
+      setStart( start + range)
+      setEnd(end + range)
+     } else {
+      setStart( 0)
+      setEnd(range)
+     }
+  }
+
+  function previous(){
+    if(start >= range){
+      setStart( start - range)
+      setEnd(end - range)
+    } else {
+      setStart( maxSlider - range)
+      setEnd(maxSlider)
+    }
+
+  }
+
+
 
   return (
-    <div>
-        
-    {arrayCity.slice(0,4).map((e)=>{
-        return(
-            <div className='Slider-container' key={e.id} >
-                <img src={e.img} />
-                <p> {e.city} </p>
-            </div>
-        )
-    })} 
+    <div className="Slider-box">
+      <ButtonCarousel icon={"<"} onClick={previous} />
 
+      <div className="Slider-container">
+      {arrayCity.slice(start, end).map((e) => {
+        return (
+          <div className="Slider-div" key={e.id}>
+              <img src={e.img} className="Slider-img" alt={e.city}  />
+              <p className="Slider-p"> {e.city}   </p>
+          </div>
+        );
+      })}
+      </div>
+      <ButtonCarousel icon={">"} onClick={next} />
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/RenderCarousel.jsx
+++ src/components/RenderCarousel.jsx
@@ -1,51 +1,54 @@
 import React from 'react'
+import ButtonCarousel from './ButtonCarousel'
 import Carousel from './Carousel'
 
 const RenderCarousel = () => {
     const itineraries = [
         {id: 1,
-            img: "https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffea8ca4f41207f3605182/previews/62ffea8da4f41207f3605194/download/cambodia.png",
+            img: "https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/cambodia.png",
         city: 'Cambodia'},
         {id: 2,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed3ec4e5b96b2a7331ce/previews/62ffed40c4e5b96b2a733213/download/baliTemple.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/baliTemple.png',
         city: 'Bali'},
         {id: 3,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed41e6fefd1ea94c21ac/previews/62ffed42e6fefd1ea94c21c6/download/boraBora.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/boraBora.png',
         city: 'Bora Bora Island'},
         {id: 4,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed4347a9264ea7483974/previews/62ffed4447a9264ea74839a1/download/canaimaBolivar.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/canaimaBolivar.png',
         city: 'Bolívar'},
         {id: 5,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed458c3d97622e640659/previews/62ffed468c3d97622e6406e2/download/japanHonshuIsland.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/japanHonshuIsland.png',
         city: 'Honshu Island'},
         {id: 6,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed4735587376e250a3b7/previews/62ffed4935587376e250a3cb/download/maldives.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/maldives.png',
         city: 'Maldives Islands'},
         {id: 7,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed4abffbfe02664a5a59/previews/62ffed4bbffbfe02664a5a86/download/peritoMorenoCalafate.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/peritoMorenoCalafate.png',
         city: 'Calafate'},
         {id: 8,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed4e78032388ee20719e/previews/62ffed4f78032388ee2071ad/download/sidney.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/sidney.png',
         city: 'Sydney'},
         {id: 9,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed5064d1975fad1be865/previews/62ffed5164d1975fad1be91b/download/tenochtitlan.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/tenochtitlan.png',
         city: 'Tenochtitlan'},
         {id: 10,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed5211b8c5538948fdb2/previews/62ffed5311b8c5538948fe5b/download/torresDelPaineMagallanesRegion.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/torresDelPaineMagallanesRegion.png',
         city: 'Magallanes Region'},
         {id: 11,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed554beeaa7785038b1c/previews/62ffed564beeaa7785038b5b/download/victoriaFallsZambia.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/victoriaFallsZambia.png',
         city: 'Zambia'},
         {id: 12,
-            img: 'https://trello.com/1/cards/62ffea84561ffc644b73fc40/attachments/62ffed4cacbfee5eaf8f0e01/previews/62ffed4dacbfee5eaf8f0eaa/download/salahCitadelCairoCity.png',
+            img: 'https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/salahCitadelCairoCity.png',
         city: 'Cairo City'},
     ]
 
   return (
-    <>
-        <h2>Popular MYtineraries</h2>
-        <Carousel cities={itineraries} />
-    </>
+    <div className='Render-div' >
+        <div className='Render-div-title'>
+            <h2 className='Carousel-title' >Popular MYtineraries</h2>
+        </div>
+        <Carousel cities={itineraries} slides={3} range={4} />
+    </div>
   )
 }
 
