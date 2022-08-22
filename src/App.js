ab2d2dda6973b6bf09723bb6a21ab606e627b22c
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-22 16:17:50 -0300

    minor updates

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
 
   return (
     <>
-    <Welcome/>
-    {/* <Cities/> */}
+    {/* <Welcome/> */}
+   <Cities/>
     {/* <UnderConstruction/> */}
     </>
   );
--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import NotAvailable from '../components/NotAvailable'
-import Layout from '../layouts/Layout'
+import React from "react";
+import NotAvailable from "../components/NotAvailable";
+import Layout from "../layouts/Layout";
 
 function UnderConstruction() {
   return (
     <>
-        <Layout>
-            <NotAvailable/>
-        </Layout>
-        
+      <Layout>
+        <NotAvailable />
+      </Layout>
     </>
-    
-  )
+  );
 }
 
-export default UnderConstruction
+export default UnderConstruction;
