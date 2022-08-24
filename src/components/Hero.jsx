4226aae8bb056a33d5ada4f8157bea913411091f
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-24 13:41:41 -0300

    creation of logo component plus hero placement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../App.css'
+import Logo from './Logo'
+import CallToAction from './CallToAction'
 
 const Hero = () => {
   return (
     <div className='Hero-container'>
         <div className='Hero-titles'>
-            <h1>MyTinerary</h1>
-            <h2>Pulp Fiction</h2>
-            <p>Make your Dream Trip come true!!!</p>
-            <button className='btn btn-dark'>Enter</button>
+            <Logo />
+            <CallToAction />
         </div>
     </div>
   )
