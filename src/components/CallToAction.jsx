019e268a52223cb0315a4bb69aa103ce0001d172
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-24 17:28:44 -0300

    update styles for CallToAction component

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -3,7 +3,9 @@ import {Link as LinkTo} from 'react-router-dom'
 
 function CallToAction() {
   return (
-    <LinkTo className='btn' to='/cities'>Enter</LinkTo>
+    <LinkTo className='btn' to='/cities'>
+      <img className='imgCallTo' src='https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/callTo.png' alt="callTo"/>
+    </LinkTo>
   )
 }
 
