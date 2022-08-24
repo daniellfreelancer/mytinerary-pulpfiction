e3ddb4d9d2c5954dbef5de2378f74f7990a26952
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-08-24 17:26:54 -0300

    slogan component created

---
+++ src/components/Slogan.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Slogan() {
+  return (
+    <div className='Slogan-div'>
+        <marquee behavior="alternate" direction="">
+            <h1 className='Slogan'>We make your Dream Trip come True...🏖️ </h1>
+        </marquee>
+        
+    </div>
+    
+  )
+}
+
+export default Slogan
