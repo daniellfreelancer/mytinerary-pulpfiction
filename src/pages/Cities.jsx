8c9f3bee6f8d26f33f4357b501597d0b24055166
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-24 13:39:05 -0300

    minor updates plus formating

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import '../App.css'
-import RenderCarousel from '../components/RenderCarousel'
 import Layout from '../layouts/Layout'
 
 const Cities = () => {
   return (
     <>
         <Layout>
-          <RenderCarousel/>
           <p>Cities and filters will be added later</p>
         </Layout>
     </>
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
+import RenderCarousel from '../components/RenderCarousel'
 
 const Welcome = () => {
   return (
     <>
     <Hero/>
+    <RenderCarousel/>
+    <Footer/>
     </>
   )
 }
