f4bbebb325e30fb7711820ecd9c9e8fd3f252e15
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-25 09:25:29 -0300

    created component for searchbar and placed in city page + styles

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import '../styles/App.css'
 import Layout from '../layouts/Layout'
 import CityCard from '../components/CityCard'
+import SearchBar from '../components/SearchBar'
 
 const Cities = () => {
   return (
     <>
         <Layout>
+          <div className='upperbox'>
+            <SearchBar/>
+          </div>
           <CityCard/>
         </Layout>
     </>
