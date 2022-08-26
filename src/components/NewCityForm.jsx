8956046b3aed8132e1f44b6e2e340aa972c087d0
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-08-26 16:35:15 -0300

    dinamic form

--- src/components/NewCityForm.jsx
+++ src/components/NewCityForm.jsx
@@ -2,6 +2,9 @@ import { React, useState } from "react";
 import "../styles/App.css";
 
 function NewCityForm() {
+
+    let arrayCities = []
+
     const [inputs, setInputs] = useState({});
 
     const handleChange = (newcity) => {
@@ -12,54 +15,75 @@ function NewCityForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(inputs);
+        arrayCities.push(inputs)
+        console.log(arrayCities)
+        let inputForm = document.querySelector("#form-new-cities")
+        inputForm.reset()
     };
 
+    const arrayForm = [
+        {
+            id:"_country",
+            name: "Country",
+            type: "text",
+            value:""
+
+        },
+        {
+            id:"_city",
+            name: "City",
+            type: "text",
+            value:""
+
+        },
+        {
+            id:"_fundation",
+            name: "Fundation",
+            type: "date",
+            value:""
+
+        },
+        {
+            id:"_photo",
+            name: "Photo",
+            type: "url",
+            value:""
+
+        },
+        {
+            id:"_population",
+            name: "Population",
+            type: "number",
+            value:""
+
+        }
+
+
+    ]
+
+    const formView = (e) => {
+        return (
+            <label key={e.id}>
+            Enter the {e.name}: <br />
+                <input
+                    className="input-text"
+                    type={e.type}
+                    name={e.name}
+                    value={inputs.value}
+                    onChange={handleChange}
+                />
+            </label>
+        )
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form id="form-new-cities" onSubmit={handleSubmit}>
             <fieldset>
                 <h2>You can enter a new city!</h2>
                 <p>Simply fill in the blanks and let us know about where you´d like to travel!</p>
-                <label>
-                    Enter the Country: <br />
-                    <input
-                        className="input-text"
-                        type="text"
-                        name="country"
-                        value={inputs.country || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    Enter a New City: <br />
-                    <input
-                        className="input-text"
-                        type="text"
-                        name="new_city"
-                        value={inputs.cityname || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    Any special location in said city: <br />
-                    <input
-                        className="input-text"
-                        type="text"
-                        name="place"
-                        value={inputs.place || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    Add a Photo: <br />
-                    <input
-                        className="input-text"
-                        type="text"
-                        name="photo"
-                        value={inputs.photo || ""}
-                        onChange={handleChange}
-                    />
-                </label>
+                {
+                    arrayForm.map(formView)
+                }
                 <input className="input-submit" type="submit" />
             </fieldset>
         </form>
