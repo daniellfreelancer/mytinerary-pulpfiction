7aec1c17484bb294aaa978ad4dffcf62e9e3fc7a
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-01 09:46:55 -0300

    added Edit City page

---
+++ src/pages/EditCity.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../layouts/Layout'
+
+function EditCity() {
+  return (
+    <Layout>
+        <p>Edit your city</p>
+    </Layout>
+  )
+}
+
+export default EditCity
