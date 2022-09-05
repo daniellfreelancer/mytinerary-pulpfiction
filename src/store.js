c1fbe356c2ed6c2f10fe292ac6c717fbf215ab3d
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-05 14:25:56 -0300

    minor modification regarding reducer from API

--- src/store.js
+++ src/store.js
@@ -1,9 +1,23 @@
-import {configureStore} from '@reduxjs/toolkit'
-
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import citiesSlice from './features/citiesSlice'
+import citiesAPI  from './features/citiesAPI'
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
-        cities: citiesSlice
+        cities: citiesSlice,
+
+        [citiesAPI.reducerPath]: citiesAPI.reducer
     },
+
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(citiesAPI.middleware),
+
+
+    
 })
+
+
+
+setupListeners(store.dispatch)
+export default store
