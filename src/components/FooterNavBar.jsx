162473791c8ce4efb10fede58bceb531c904ca50
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-12 18:41:50 -0300

    paths to registration + scripts

--- src/components/FooterNavBar.jsx
+++ src/components/FooterNavBar.jsx
@@ -6,6 +6,7 @@ const pages = [
   { id: "_cities", to: "/cities", title: "Cities" },
   { id: "_newCities", to: "/newCities", title: "New Cities" },
   { id: "_myTinerary", to: "/myTineraries", title: "My Tineraries" },
+  { id: "_registration", to: "/auth", title: "Registration" },
 ];
 
 export function NavBarFooter() {
