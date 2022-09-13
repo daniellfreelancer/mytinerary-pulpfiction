968798bef1dcf2d064c3b599746e87f0069967ed
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-13 20:42:07 -0300

    Added Signin and SignOut Redux

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,52 +1,84 @@
 import React, { useRef } from 'react'
+import { useSignInUserMutation, useSignOutUserMutation } from '../features/userAPI';
 import SignInGoogle from './SignInGoogle'
 
 function SignInForm() {
 
     const passwordUserRef = useRef()
     const emailUserRef = useRef()
+    const [signInUser] = useSignInUserMutation();
+    const [signOutUser] = useSignOutUserMutation();
 
-
-
-
-    //const [addNewUser] = useSignUpMutation();
-
+    
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        let userFrom = 'form'
+        let userFrom = 'google'
 
         let newUser = {
-            password: passwordUserRef.current.value,
+            pass: passwordUserRef.current.value,
             email: emailUserRef.current.value,
             from: userFrom
         };
-        console.log(newUser)
+
+        signInUser(newUser)
+          .then((res) => {
+
+            console.log(res)
+            
+          localStorage.setItem('testUser', JSON.stringify(res.data.response.user));
+
+        })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        let signupForm = document.querySelector("#form-new-users");
+        signupForm.reset();
+    }
+    let variableTest
+
+    if(localStorage.length === 0) {
+        console.log("local esta vacio")
+    } else {
+        variableTest =  JSON.parse(localStorage.getItem('testUser'))
     }
 
-    //   addNewUser(newUser)
-    //       .then((res) => console.log(res))
-    //       .catch((error) => {
-    //         console.log(error);
-    //       });
+    console.log(variableTest)
 
-    //       let signupForm = document.querySelector("#form-new-users");
-    //       signupForm.reset();
-    //     };
+     const handleSignOut = (e) =>{
+        e.preventDefault();
 
+        let userMail = {
+            email:variableTest.email
+        }
+
+        console.log(userMail)
+
+        signOutUser(userMail)
+        .then((res) =>console.log(res))
+        .catch((error) => {
+          console.log(error);
+        });
+
+        localStorage.clear()
+     }
+
+    
 
     const arrayForm = [
-        {
-            id: "_password",
-            name: "Password",
-            type: "text",
-            value: passwordUserRef,
-        },
+
         {
             id: "_email",
             name: "Email",
             type: "email",
             value: emailUserRef,
+        },
+        {
+            id: "_password",
+            name: "Password",
+            type: "text",
+            value: passwordUserRef,
         }
     ];
 
@@ -64,23 +96,34 @@ function SignInForm() {
         );
     };
     return (
-        <form id="form-new-users" onSubmit={handleSubmit}>
-            <fieldset>
-                <h2>Here you can Sign In your account!</h2>
-                <p>
-                    Simply fill in the blanks
-                </p>
+      <form id="form-new-users" onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Here you can Sign In your account!</h2>
+          <p>Simply fill in the blanks</p>
 
-                {arrayForm.map(formView)}
+          {arrayForm.map(formView)}
 
-                <input className="input-submit" type="submit" value="Submit" />
+          <input className="input-submit" type="submit" value="Submit" />
+        </fieldset>
+        <fieldset>
+          <SignInGoogle />
 
-            </fieldset>
-            <fieldset>
-                <SignInGoogle />
-            </fieldset>
-        </form>
-    )
+          <br />
+          {localStorage.length !== 0 ? (
+                      <div key={variableTest.id}>
+                      <button onClick={handleSignOut}>sign Out</button>
+                      <p> {variableTest.id} </p>
+                      <p> {variableTest.name} </p>
+                      <p> {variableTest.email} </p>
+                      <img src={variableTest.photo} alt={variableTest.name} />
+                    </div>
+          ) : (
+            <button>No está logueado</button>
+          )}
+
+        </fieldset>
+      </form>
+    );
 }
 
 export default SignInForm
