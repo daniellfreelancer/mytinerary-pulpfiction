378ea13d57ed298090af2a62edd1f7bd331641d4
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-14 12:34:54 -0300

    Added Alert component for newCity, SignIn, SignUp and Sign Out

--- src/components/NewCityForm.jsx
+++ src/components/NewCityForm.jsx
@@ -1,7 +1,8 @@
 //import axios from "axios";
-import { React, useRef } from "react";
+import { React, useRef, useState } from "react";
 import "../styles/App.css";
 import { useCreateCityMutation } from "../features/citiesAPI";
+import AlertComponent from "./AlertComponent";
 
 function NewCityForm() {
   const cityPhotoRef = useRef();
@@ -13,7 +14,14 @@ function NewCityForm() {
   const featuredLocationRef = useRef();
   const smallDetailsRef = useRef();
 
-  const [addNewCity] = useCreateCityMutation();
+  const [addNewCity, isError, isSuccess, error] = useCreateCityMutation();
+
+  // states
+  const [modalOpen, setModalOpen] = useState(false);
+  const [messageError, setMessageError] = useState("")
+  const [messageTittle, setMessageTittle] = useState("")
+  const [iconSVG, setIconSVG] = useState("")
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,17 +37,47 @@ function NewCityForm() {
       smalldescription: smallDetailsRef.current.value,
     };
 
-    console.log(newCity);
-
     addNewCity(newCity)
-      .then((res) => console.log(res))
+      .then((res) =>{
+        
+        if (res.error){
+
+        let dataError = res.error
+        let dataMessage = dataError.data
+        //alert(dataMessage.message)
+        setModalOpen(true)
+        setMessageError(dataMessage.message)
+        setMessageTittle("Error")
+        setIconSVG(<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
+        <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </svg>)
+
+        } else {
+
+          let dataResponse = res.data
+          let dataSuccess = dataResponse.message
+         setModalOpen(true)
+         setMessageError(dataSuccess)
+         setMessageTittle("Success")
+         setIconSVG(<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fillRule="currentColor" className="bi bi-check-square-fill" viewBox="0 0 16 16">
+         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
+       </svg>)
+          //alert(dataSuccess)
+          let inputForm = document.querySelector("#form-new-cities");
+          inputForm.reset();
+
+        }
+        
+      } )
       .catch((error) => {
         console.log(error);
       });
 
-    let inputForm = document.querySelector("#form-new-cities");
-    inputForm.reset();
+
   };
+
+
+
 
   const arrayForm = [
     {
@@ -106,8 +144,25 @@ function NewCityForm() {
     );
   };
 
+  
+
   return (
+    <>
+   
+    
     <form id="form-new-cities" onSubmit={handleSubmit}>
+
+    {modalOpen == true ?
+     <AlertComponent 
+     setOpenModal={setModalOpen} 
+     setMessageError={messageError}
+     setMessageTittle={messageTittle}
+     setIconSVG={iconSVG}
+     /> : 
+     null}
+
+    <div></div>
+      
       <fieldset>
         <h2>You can enter a new city!</h2>
         <p>
@@ -120,6 +175,7 @@ function NewCityForm() {
         <input className="input-submit" type="submit" value="Submit" />
       </fieldset>
     </form>
+    </>
   );
 }
 export default NewCityForm;
