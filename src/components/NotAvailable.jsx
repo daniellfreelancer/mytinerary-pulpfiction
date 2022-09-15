2753e05733308551871ade1b88a2f4b9e67f8e79
cindy-fdb <109637262+cindy-fdb@users.noreply.github.com>
2022-09-15 11:14:30 -0300

    img hosting changed

--- src/components/NotAvailable.jsx
+++ src/components/NotAvailable.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NotAvailable() {
   return (
     <div className='NotAvailable'>
-         <img className="img404" src="https://raw.githubusercontent.com/daniellfreelancer/citiesCloudImg/main/404TwoSmall.png" alt="underConstrution" />
+      <img className="img404" src="https://i.im.ge/2022/09/15/1rE4by.404TwoSmall.png" alt="underConstrution" />
     </div>
   )
 }
