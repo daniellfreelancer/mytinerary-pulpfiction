abb086535dc9c09dd2eac2f2aaa0954788dcece4
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-20 09:58:16 -0300

    update api_url for use localhost

--- src/features/itineraryAPI.js
+++ src/features/itineraryAPI.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import api_url from '../api'
 
 export const itineraryAPI = createApi({
 
@@ -6,7 +7,7 @@ export const itineraryAPI = createApi({
 
 
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:4000'
+        baseUrl: api_url
     }),
 
 
