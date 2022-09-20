60ea79d570e9377dabcf5a258b9a8da9b988d655
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-20 14:27:01 -0300

    updates for show links

--- src/components/FooterNavBar.jsx
+++ src/components/FooterNavBar.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkRouter } from "react-router-dom";
 import "../styles/App.css";
 
-const pages = [
-  { id: "_cities", to: "/cities", title: "Cities" },
-  { id: "_newCities", to: "/newCities", title: "New Cities" },
-  { id: "_myTinerary", to: "/myTineraries", title: "My Tineraries" }
-];
+
 
 export function NavBarFooter() {
+  const pages = [
+    { id: "_cities", to: "/cities", title: "Cities" }
+  ];
+  
+  const [statusAccount, setStatusAccount] = useState(false)
+  const [myStatusLogged, setMyStatusLogged] = useState(
+    [
+    {
+      id: "_myAccount",
+      to: "/myAccount", 
+      title: "My Account"
+    },
+    { 
+      id: "_newCities", 
+      to: "/newCities", 
+      title: "New Cities" 
+    },
+    { 
+      id: "_myTinerary", 
+      to: "/myTineraries", 
+      title: "My Tineraries" 
+    }
+  ]
+  )
+  
+  
+  useEffect(() => {
+    if( localStorage.length > 0){
+      setStatusAccount(true)
+    } 
+  }, [statusAccount])
+
   return (
     <nav className="Nav-Footer">
       {pages.map((link) => (
@@ -16,6 +44,14 @@ export function NavBarFooter() {
           {link.title}
         </LinkRouter>
       ))}
+              {
+          statusAccount === true ?
+            (myStatusLogged.map((link) => (
+              <LinkRouter className="footer-navlink" to={link.to} key={link.id}>
+                {link.title}
+              </LinkRouter>
+            ))) : null
+        }
     </nav>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,21 +3,32 @@ import { Link as LinkRouter } from "react-router-dom";
 import "../styles/App.css";
 
 
-
-
-
-
 export function NavBar() {
   const pages = [
-    { id: "_cities", to: "/cities", title: "Cities" },
-    { id: "_newCities", to: "/newCities", title: "New Cities" },
-    { id: "_myTinerary", to: "/myTineraries", title: "My Tineraries" }
+    { id: "_cities", to: "/cities", title: "Cities" }
   ];
 
   const [statusAccount, setStatusAccount] = useState(false)
-  const [myStatusLogged, setMyStatusLogged] = useState({
-    id: "_myAccount", to: "/myAccount", title: "My Account"
-  })
+
+  const pagesLogged = [
+    {
+      id: "_myAccount",
+      to: "/myAccount", 
+      title: "My Account"
+    },
+    { 
+      id: "_newCities", 
+      to: "/newCities", 
+      title: "New Cities" 
+    },
+    { 
+      id: "_myTinerary", 
+      to: "/myTineraries", 
+      title: "My Tineraries" 
+    }
+  ]
+
+
   
 
   useEffect(() => {
@@ -39,10 +50,13 @@ export function NavBar() {
           </LinkRouter>
         ))}
         {
-          statusAccount == true ?
-            (<LinkRouter className="navlink" to={myStatusLogged.to} key={myStatusLogged.id}>
-          {myStatusLogged.title}
-        </LinkRouter>) : null
+          statusAccount === true ? (
+            pagesLogged.map((link) => (
+              <LinkRouter className="navlink" to={link.to} key={link.id}>
+                {link.title}
+              </LinkRouter>
+            ))
+          ): null
         }
       </nav>
     </>
