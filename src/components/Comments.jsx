8c0c7b771eb54b2f03dbb79bb26b4edc4a23c110
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-21 15:18:46 -0300

    update coments component

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
-import { Link as LinkRouter } from "react-router-dom";
-import LogIn from "./LogIn";
-function Comments() {
+import { useGetCommentsQuery } from "../features/commentAPI";
+import { useDispatch, useSelector } from "react-redux";
+import { setStateLogin } from "../features/stateLocalStorage";
+
+function Comments(props) {
+  let idItinerary = props.id;
+  const { data: comments, isSuccess } = useGetCommentsQuery(idItinerary);
+  let commentsItinerary = comments?.response;
   const [show, setShow] = useState(false);
+
+  const loginStateRedux = useSelector((state) => state.statesLocalStorage);
+  const dispatch = useDispatch();
+  if (JSON.parse(localStorage.getItem("testUser"))) {
+    dispatch(setStateLogin(true));
+  } else {
+    dispatch(setStateLogin(false));
+  }
 
   function showComment() {
     if (show) {
@@ -13,96 +26,66 @@ function Comments() {
   }
 
   return (
-    <div className="Comment-div">
-      <button className="Comment-button" onClick={showComment}>
-      Comments
-        {" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="25"
-          height="25"
-          className="bi bi-chat-heart"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2.965 12.695a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2Zm-.8 3.108.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125ZM8 5.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"
-          />
-        </svg>{" "}
-        
-      </button>
-      <div>
-        {show ? (
+    <div className="comment-box">
+      <div className="Comment-div">
+        <button className="Comment-button" onClick={showComment}>
+          Comments{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            className="bi bi-chat-heart"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.965 12.695a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2Zm-.8 3.108.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125ZM8 5.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"
+            />
+          </svg>{" "}
+        </button>
+        <div>
+          {show ? (
+            <>
+              {isSuccess ? (
+                commentsItinerary?.map((c) => {
+                  return (
+                    <div className="comment-container" key={c._id}>
+                      <div className="side-comment"></div>
+                      <div className="entercomment">
+                        <div className="comment-avatar">
+                          <img src={c.user.photo} alt={c.user.name} />
+                          <h5>{c.user.name}</h5>
+                          <p>{c.user.role}</p>
+                        </div>
+                        <div className="comment-form">
+                          <p>{c.comment}</p>
 
-<div className="comments-container">
-    <ul id="comments-list" className="comments-list">
-    <li>
-      <div className="comment-main-level">
-        <div className="comment-avatar"><img src="https://i.pinimg.com/originals/42/04/77/4204771e1c54c2b9afa36f668261e7bd.gif" alt="user"/></div>
-        <div className="comment-box">
-          <div className="comment-head">
-            <h6 className="comment-name by-author">Cinthya</h6>
-            <i className="fa fa-reply"></i>
-            <i className="fa fa-heart"></i>
-          </div>
-          <div className="comment-content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
-          </div>
+                          <p>{c.itinerary}</p>
+
+                          {loginStateRedux ? (
+                            <div className="button-box-c">
+                              <button className="button-comment-bottom">
+                                Edit
+                              </button>
+                              <button
+                                id={c._id}
+                                className="button-comment-bottom"
+                              >
+                                Detele
+                              </button>
+                            </div>
+                          ) : null}
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <p>No Comments Yet</p>
+              )}
+            </>
+          ) : null}
         </div>
-      </div>
-      <ul className="comments-list reply-list">
-        <li>
-          <div className="comment-avatar"><img src="https://i.pinimg.com/originals/19/14/e1/1914e17788de73ac8423e983105c54c1.gif" alt="user"/></div>
-          <div className="comment-box">
-            <div className="comment-head">
-              <h6 className="comment-name">Natalia</h6>
-              <i className="fa fa-reply"></i>
-              <i className="fa fa-heart"></i>
-            </div>
-            <div className="comment-content">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
-            </div>
-          </div>
-        </li>
-
-        <li>
-         
-          <div className="comment-avatar"><img src="https://i.pinimg.com/originals/42/04/77/4204771e1c54c2b9afa36f668261e7bd.gif" alt="user"/></div>
-          
-          <div className="comment-box">
-            <div className="comment-head">
-              <h6 className="comment-name by-author">Cinthya</h6>
-              <i className="fa fa-reply"></i>
-              <i className="fa fa-heart"></i>
-            </div>
-            <div className="comment-content">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
-            </div>
-          </div>
-        </li>
-      </ul>
-    </li>
-
-    <li>
-      <div className="comment-main-level">
-       
-        <div className="comment-avatar"><img src="https://www.cinemascomics.com/wp-content/uploads/2020/06/poder-darth-vader.jpg" alt="user"/></div>
-        
-        <div className="comment-box">
-          <div className="comment-head">
-            <h6 className="comment-name by-author">Daniel</h6>
-            <i className="fa fa-reply"></i>
-            <i className="fa fa-heart"></i>
-          </div>
-          <div className="comment-content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
-</div>
-        ) : null}
       </div>
     </div>
   );
