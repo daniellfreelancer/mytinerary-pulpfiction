ada1e187afee36def29a4e204d20680b9cc9ff91
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-09-23 15:59:44 -0300

    update api_url for hosting

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-const api_url = 'https://my-tinerary-back-pulpfiction.herokuapp.com'
+//const api_url = 'https://my-tinerary-back-pulpfiction.herokuapp.com'
 //const api_url = 'http://localhost:4000'
+const api_url = 'https://pulp-fiction-oficial.herokuapp.com'
 
 
 export default api_url
